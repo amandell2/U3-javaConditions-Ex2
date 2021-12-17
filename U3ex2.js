1efbe57ed13baaf739e97dcd5a9567e32fe9e70d
Amandell <amanda.mandell2@gmail.com>
2021-12-16 21:17:16 -0500

    changed == to ===

--- U3ex2.js
+++ U3ex2.js
@@ -19,19 +19,19 @@ else if(length3 > length1 && length3 > length2){
     console.log(`${name3} has the longest name.`);
 }
 /*name1 & name2 are tied and longest*/ 
-else if(length1 == length2 && length1 > length3){
+else if(length1 === length2 && length1 > length3){
     console.log(`${name1} and ${name2} tie for the longest name.`);
 }
 /*name1 & name3 are tied*/ 
-else if(length1 == length3 && length1 > length2){
+else if(length1 === length3 && length1 > length2){
     console.log(`${name1} and ${name3} tie for the longest name.`);
 }
 /*name2 & name3 are tied*/ 
-else if(length2 == length3 && length2 > length1){
+else if(length2 === length3 && length2 > length1){
     console.log(`${name2} and ${name3} tie for the longest name.`);
 }
 /*all 3 are tied*/
-else if(length1 == length2 && length1 == length3){
+else if(length1 === length2 && length1 === length3){
     console.log(`All three names, ${name1}, ${name2}, and ${name3}, are the same length.`);
 }
 
